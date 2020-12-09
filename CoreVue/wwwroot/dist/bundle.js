8cd744ff525273eb6fdffa13ef731a20745609de
chalz777 <charlesnam37@hotmail.com>
2020-12-09 10:50:05 -0800

    vue cli install and add vuetify

--- CoreVue/wwwroot/dist/bundle.js
+++ CoreVue/wwwroot/dist/bundle.js
@@ -150,7 +150,7 @@ eval("module.exports = function(module) {\n\tif (!module.webpackPolyfill) {\n\t\
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"./node_modules/lodash/lodash.js\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n\n\nfunction createChild() {\n    var element = document.createElement('div');\n    element.innerHTML = lodash__WEBPACK_IMPORTED_MODULE_0___default.a.join(['Hello', 'Webpack'], ' ');\n    return element;\n}\n\ndocument.body.appendChild(createChild());\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"./node_modules/lodash/lodash.js\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n\r\n\r\nfunction createChild() {\r\n    var element = document.createElement('div');\r\n    element.innerHTML = lodash__WEBPACK_IMPORTED_MODULE_0___default.a.join(['Hello', 'Webpack'], ' ');\r\n    return element;\r\n}\r\n\r\ndocument.body.appendChild(createChild());\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ }),
 

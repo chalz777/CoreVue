d13a173799c073a0caeb615e10e8b45a3499b0e5
chalz777 <charlesnam37@hotmail.com>
2021-03-23 17:11:15 -0700

    Clean out for font

--- CoreVue/Startup.cs
+++ CoreVue/Startup.cs
@@ -31,6 +31,7 @@ namespace CoreVue
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +47,17 @@ namespace CoreVue
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
